a51c0f43a92c9a1d7cebf5865140634855a463dd
Jason Champion <xangis@Jasons-MBP.domain>
2019-07-13 10:49:20 -0700

    Add printing loaded libraries to showlibs command.

--- installer/OSXCopyLibs.sh
+++ installer/OSXCopyLibs.sh
@@ -11,4 +11,4 @@ cp /usr/local/lib/libwx_baseu_xml-3.1.dylib DrumPads.app/Contents/Frameworks/
 cp /usr/local/lib/libwx_osx_cocoau_xrc-3.1.dylib DrumPads.app/Contents/Frameworks/
 cp /usr/local/lib/libwx_baseu-3.1.dylib DrumPads.app/Contents/Frameworks/
 cp ../../../lib/SDL2_ttf-2.0.15/.libs/libSDL2_ttf-2.0.0.dylib DrumPads.app/Contents/Frameworks/
-cp ../../..//lib/rtmidi-4.0.0/.libs/librtmidi.5.dylib DrumPads.app/Contents/Frameworks/
+cp ../../../lib/rtmidi-4.0.0/.libs/librtmidi.5.dylib DrumPads.app/Contents/Frameworks/
